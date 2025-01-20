f8ea6ab85e9ec1e702638c0f7a9a7ad0d5c083f4
val-vgs <144251822+val-vgs@users.noreply.github.com>
2025-01-20 01:04:27 -0600

    Update __init__.py with proper exports

--- tfanalyzer/__init__.py
+++ tfanalyzer/__init__.py
@@ -1,6 +1,8 @@
-from .analyzer import TerraformAnalyzer
-from .parser import TerraformParser
+"""Terraform module analyzer package."""
+from .models import ModuleInfo, ResourceInfo, TagAnalysis
+from .resources import REQUIRED_TAGS, TAGGABLE_RESOURCES
+from .analyzer import ModuleAnalyzer
 
 __version__ = '0.1.0'
-
-__all__ = ['TerraformAnalyzer', 'TerraformParser']
+__all__ = ['ModuleAnalyzer', 'ModuleInfo', 'ResourceInfo', 'TagAnalysis', 
+           'REQUIRED_TAGS', 'TAGGABLE_RESOURCES']
